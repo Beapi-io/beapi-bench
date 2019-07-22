bc9b36e8827951e728e31b2e55859d2cbecdb801
Owen Rubel <orubel@gmail.com>
2019-07-22 15:20:20 -0700

    saving changes

--- BeapiBench.groovy
+++ BeapiBench.groovy
@@ -17,18 +17,7 @@ import java.text.DecimalFormat
  *  -testTime=60 (seconds)
  */
 class BeapiBench {
-    Integer testSize = 75
-    Integer testTime = 60
-    String login = 'admin'
-    String password = 'Al1c3Inj@1L'
-    String loginUri = '/api/login'
-    String apiToken
-    String testDomain = 'http://localhost:8080'
-    //List concurrency = [50, 50, 75, 75, 100, 100, 125, 125, 150, 150, 200, 200, 250, 250, 300, 300, 350, 350, 400, 400]
-    //List requests = [1000, 1500, 2000, 2500, 3000, 4000, 5000, 7500, 8000, 10500, 13000, 1700, 21000, 27500, 34000, 44500, 55000, 72000, 89000, 116500]
-    String help = """
 
-"""
 
     static void main(String[] args) {
         CommandLineInterface cli = CommandLineInterface.INSTANCE
@@ -42,17 +31,23 @@ class BeapiBench {
  */
 enum CommandLineInterface{
     INSTANCE
-    List methods = ['GET','PUT','POST','DELETE']
-    String method
-    String endpoint
-    Integer concurrency = 50
-    Integer requests = 1000
-    String token
-    List headers
-    String contentType = 'application/json'
 
-    String tempPath = '/tmp'
-    String fileName = 'beapiBench.txt'
+    private List methods = ['GET', 'PUT', 'POST', 'DELETE']
+    protected String method
+    protected String endpoint
+    protected Integer concurrency = 50
+    protected Integer requests = 1000
+    protected String token
+    protected List headers
+    protected String contentType = 'application/json'
+
+    protected String path = '/tmp/'
+    protected String filename = 'beapiBench.txt'
+    protected String tmpPath = path+filename
+
+    Integer testSize = 75
+    Integer testTime = 60
+
 
     CliBuilder cliBuilder
 
@@ -84,7 +79,7 @@ enum CommandLineInterface{
         }
     }
 
-    void parse(args){
+    void parse(args) {
         OptionAccessor options = cliBuilder.parse(args)
         try {
             if (!options) {
@@ -95,115 +90,115 @@ enum CommandLineInterface{
                 System.exit 0
             }
 
-            if(options.m && options.endpoint){
-                if(!methods.contains(options.m.toUpperCase())){
+            if (options.m && options.endpoint) {
+                if (!methods.contains(options.m.toUpperCase())) {
                     throw new Exception('Request method not supported. Please try again.\n')
                 }
                 this.method = options.m
 
 
                 try {
-                    new URL(endpoint)
+                    URL url = new URL(options.endpoint)
                 } catch (Exception e) {
-                    throw new Exception('Endpoint is not a valid URL. Please try again',e)
+                    throw new Exception('Endpoint is not a valid URL. Please try again', e)
                 }
                 this.endpoint = options.endpoint
-            }else {
+            } else {
                 throw new Exception('Method (--method) and Endpoint (--endpoint) are both REQUIRED for BeapiBench to work. Please try again.\n')
             }
-
 
 
             if (options.c) {
                 try {
                     this.concurrency = options.c as Integer
-                }catch(Exception e){
-                    throw new Exception('Concurrency (--concurrency ,-c) expects an Integer. Please try again.',e)
-                }
-            }
-            if (options.n) {
-                try {
-                    this.requests = options.n as Integer
-                }catch(Exception e){
-                    throw new Exception('Requests (--request, -n) expects an Integer. Please try again.',e)
+                    if (!(this.concurrency>0)) {
+                        throw new Exception('Concurrency must be a positive number greater than 0. Please try again.')
+                    }
+                } catch (Exception e) {
+                    throw new Exception('Concurrency (--concurrency ,-c) expects an Unsigned Integer greater than 0. Please try again.', e)
                 }
             }
 
-            if(this.concurrency>=this.requests){
+            if (options.n) {
+                try {
+                    this.requests = options.n as Integer
+                    if (!(this.requests>0)) {
+                        throw new Exception('Number of requests must be a positive number greater than 0. Please try again.')
+                    }
+                } catch (Exception e) {
+                    throw new Exception('Requests (--request, -n) expects an Unsigned Integer greater than 0. Please try again.', e)
+                }
+            }
+
+            if (this.concurrency >= this.requests) {
                 throw new Exception('Concurrency must be less than number of requests. Please try again')
             }
 
             if (options.t) {
-                this.token = options.t.trim())
+                this.token = options.t.trim()
             }
             if (options.p) {
-                options.p.each(){
+                options.p.each() {
                     this.headers.add(it.trim())
                 }
             }
             if (options.c) {
                 this.contentType = options.c.trim()
             }
-        }catch(Exception e){
+        } catch (Exception e) {
             System.err << e
             System.exit 1
         }
 
-/*
-        String filename = args[0]
-        BeapiBench bench = new BeapiBench()
-        bench.initToken()
-        if(bench.getData()){
-            int i = 0
-            List data = []
-            while(i<bench.testSize){
-                //String c = bench.concurrency.get(it)
-                //String n = bench.requests[it]
-                String method = "GET"
-                String controller = 'person'
-                String action = 'show'
-                String url = "${bench.testDomain}/v1.3.0/${controller}/${action}?id=225"
 
+        int i = 0
+        List data = []
+        while (i < this.testSize) {
+            // call method; move this to method
+            List returnData = callApi(this.concurrency, this.requests, this.contentType, this.token, this.method, this.endpoint)
+            if (data.size() > 0) {
+                DecimalFormat df = new DecimalFormat("0.00")
+                int size = data.size() - 1
 
-                // call method; move this to method
-                List returnData = bench.callApi(url, bench.apiToken, method)
-                if(data.size()>0){
-                    DecimalFormat df = new DecimalFormat("0.00")
-                    int size = data.size()-1
-                    Float floatTemp1 =  Float.parseFloat(returnData[1])
-                    Float floatTemp2 = Float.parseFloat(data[size][0])
-                    String floatTemp3 = Float.sum(floatTemp1,floatTemp2) as String
-                    //Float result = df.format(floatTemp3)
-                    List temp = [floatTemp3,returnData[2]]
-                    data.add(temp)
-                }else{
-                    List temp = [returnData[1],returnData[2]]
-                    data.add(temp)
-                }
-
-                float waitTime = Float.parseFloat(returnData[1])
-                waitTime = (waitTime*1000)
-                sleep(waitTime as Integer)
-
-                i++
+                // will fail here if tests fail; need to create a way to continue
+                Float floatTemp1 = Float.parseFloat(returnData[1])
+                Float floatTemp2 = Float.parseFloat(data[size][0])
+                String floatTemp3 = Float.sum(floatTemp1, floatTemp2) as String
+                //Float result = df.format(floatTemp3)
+                List temp = [floatTemp3, returnData[2]]
+                data.add(temp)
+            } else {
+                List temp = [returnData[1], returnData[2]]
+                data.add(temp)
             }
 
-            // output to file
-            // TODO: check if file exists and ask if they want to overwrite if it does
-            def apiBenchData = new File(filename)
-            apiBenchData.append('# X   Y\n')
-            data.each() {
-                apiBenchData.append '   '
-                apiBenchData.append it.join('   ')
-                apiBenchData.append '\n'
-            }
-            //println(apiBenchData.text)
+            float waitTime = Float.parseFloat(returnData[1])
+            waitTime = (waitTime * 1000)
+            sleep(waitTime as Integer)
+
+            i++
         }
-        */
+
+        // output to file
+        // TODO: check if file exists and ask if they want to overwrite if it does
+        def apiBenchData = new File(this.tmpPath)
+        if (apiBenchData.exists() && apiBenchData.canRead()) {
+            apiBenchData.delete()
+        }
+        // do I have to recreate apiBenchData???
+
+        apiBenchData.append('# X   Y\n')
+        data.each() {
+            apiBenchData.append '   '
+            apiBenchData.append it.join('   ')
+            apiBenchData.append '\n'
+        }
+        //println(apiBenchData.text)
+        createChart()
     }
 
-    protected List callApi(String url, String token, String method){
-        String bench = "ab -c 100 -n 5000 -H 'Content-Type: application/json' -H'Authorization: Bearer ${token}' -m ${method} ${url}"
+    protected List callApi(Integer concurrency, Integer requests, String contentType, String token, String method, String endpoint){
+        String bench = "ab -c ${concurrency} -n ${requests} -H 'Content-Type: ${contentType}' -H'Authorization: Bearer ${token}' -m ${method} ${endpoint}"
         def proc = ['bash', '-c', bench].execute()
         proc.waitFor()
         DecimalFormat df = new DecimalFormat("0.00")
@@ -211,7 +206,7 @@ enum CommandLineInterface{
         def error = new StringWriter()
         proc.waitForProcessOutput(outputStream, error)
         String output = outputStream.toString()
-        List<String> returnData = []
+        List<String> returnData = [0,0,0]
         if (output) {
             List lines = output.readLines()
             lines.each(){ it2 ->
@@ -220,15 +215,15 @@ enum CommandLineInterface{
                     String temp2 = temp[1].trim()
                     List temp3 = temp2.split(' ')
                     Float temp4 = Float.parseFloat(temp3[0])
-                    returnData.add(df.format(temp4))
-                    print("${df.format(temp4)}/")
+                    returnData[0] = df.format(temp4)
+                    print("[running] : ${df.format(temp4)}/")
                 }
                 if (it2 =~ /Time taken for tests/) {
                     List temp = it2.split(':')
                     String temp2 = temp[1].trim()
                     List temp3 = temp2.split(' ')
                     Float temp4 = Float.parseFloat(temp3[0])
-                    returnData.add(df.format(temp4))
+                    returnData[1] = df.format(temp4)
                     print("${df.format(temp4)}/")
                 }
                 if (it2 =~ /Requests per second/) {
@@ -236,7 +231,7 @@ enum CommandLineInterface{
                     String temp2 = temp[1].trim()
                     List temp3 = temp2.split(' ')
                     Float temp4 = Float.parseFloat(temp3[0])
-                    returnData.add(df.format(temp4))
+                    returnData[2] = df.format(temp4)
                     println("${df.format(temp4)}")
                 }
             }
@@ -247,34 +242,21 @@ enum CommandLineInterface{
     }
 
 
-
-    boolean getData() {
-        String METHOD = "GET"
-        String controller = 'person'
-        String action = 'show'
-        LinkedHashMap info = [:]
-
-        def proc = ["curl", "-H", "Content-Type: application/json", "-H", "Authorization: Bearer ${this.apiToken}", "--request", "${METHOD}", "${this.testDomain}/v1.3.0/${controller}/${action}?id=225"].execute()
-        proc.waitFor()
-        def outputStream = new StringBuffer()
-        def error = new StringWriter()
-        proc.waitForProcessOutput(outputStream, error)
-        String output = outputStream.toString()
-
-        def slurper = new JsonSlurper()
-        slurper.parseText(output).each() { k, v ->
-            info[k] = v
-        }
-
-        if(info != [:]) {
-            return true
-        }else{
-            return false
+    protected void createChart(){
+        println('createchart called...')
+        try{
+            println(this.tmpPath)
+            String bench = "gnuplot -p -e \"plot '${this.tmpPath}' using 1:2 with linespoint pt 7\""
+            def proc = ['bash', '-c', bench].execute()
+            proc.waitFor()
+            def outputStream = new StringBuffer()
+            def error = new StringWriter()
+            proc.waitForProcessOutput(outputStream, error)
+        } catch (Exception e) {
+            throw new Exception('Error creating chart. Exception follows:', e)
         }
     }
 }
-
-
 
 
 
